744edcce2691a42b3b1f69b9e8dbe730368a74d9
azakhodyakina <azakhodyakina@egartech.com>
2023-07-17 11:57:33 +0400

    first commit

---
+++ src/main/java/com/example/grarary/GrararyApplication.java
@@ -0,0 +1,13 @@
+package com.example.grarary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrararyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrararyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/grarary/GrararyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grarary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrararyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
